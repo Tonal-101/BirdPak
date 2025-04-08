4ad6aebd40fe2dd0d179cba981797ba542f64892
Tonal-101 <132414674+Tonal-101@users.noreply.github.com>
2025-04-08 00:39:16 -0700

    Update Temperature_Sensor.h

--- Temperature_Sensor.h
+++ Temperature_Sensor.h
@@ -8,10 +8,13 @@
 
 class Temperature_Sensor {
   public:
+    Temperature_Sensor(uint8_t sensorAddress, int highTempAlarm, int lowTempAlarm);
 
   private:
     void _initializeSensors();
-    
+    int _highTempAlarm;
+    int _lowTempAlarm;
+    uint8_t _sensorAddress;
 };
 
 #endif
